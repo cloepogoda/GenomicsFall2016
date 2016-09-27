615197597ffe00c3cd4ced72066811624162a1a1
ReeseBeeler <rebe4957@colorado.edu>
2016-09-27 13:09:41 -0600

    my assembler program

--- assembler.sh
+++ assembler.sh
@@ -1,1 +1,20 @@
-This is a program
+#Index reference fasta file and align.fq sequences to it
+for file in *.fa
+do
+    echo "Current fasta is $file" 
+    bwa index $file
+    for sequence in *.fq
+    do
+        echo "Current fastq library is $sequence"
+        bwa mem $file $sequence > ${sequence}.sam
+	#Convert sam to bam 
+	samtools view -b -o ${sequence}.bam -S ${sequence}.sam
+	samtools sort ${sequence}.bam ${sequence}.sorted
+	samtools index ${sequence}.sorted.bam
+	samtools faidx $file
+    done
+done
+
+#Create vcf table for all the individuals
+samtools mpileup -gf $file *.sorted.bam | bcftools view -vc - > snps_indels.vcf 
+
